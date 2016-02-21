4262231b547dcb7a2e41f3c273dda29cf939d5ba
Ernane Sena <ernane.sena@gmail.com>
2016-02-21 16:48:53 -0300

    Update indent

--- libs/config.development.js
+++ libs/config.development.js
@@ -1,17 +1,17 @@
 module.exports = {
-  database: "ntask",
-  username: "",
-  password: "",
-  params: {
-    dialect: "sqlite",
-    storage: "ntask.sqlite",
-    define: {
-      logging: false,
-      underscored: true
-    }
-  },
-  jwtSecret: "Nta$K-AP1",
-  jwtSession: {
-    session: false
-  }
+	database: "ntask",
+	username: "",
+	password: "",
+	params: {
+		dialect: "sqlite",
+		storage: "ntask.sqlite",
+		define: {
+			logging: false,
+			underscored: true
+		}
+	},
+	jwtSecret: "Nta$K-AP1",
+	jwtSession: {
+		session: false
+	}
 };
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,83 +1,83 @@
 module.exports = app => {
-  const Tasks = app.db.models.Tasks;
+	const Tasks = app.db.models.Tasks;
 
-  app.route("/tasks")
-    .all(app.auth.authenticate())
-    .get((req, res) => {
-      Tasks.findAll({
-          where: {
-            user_id: req.user.id
-          }
-        })
-        .then(result => res.json(result))
-        .catch(error => {
-          res.status(412).json({
-            msg: error.message
-          });
-        });
-    })
+	app.route("/tasks")
+		.all(app.auth.authenticate())
+		.get((req, res) => {
+			Tasks.findAll({
+					where: {
+						user_id: req.user.id
+					}
+				})
+				.then(result => res.json(result))
+				.catch(error => {
+					res.status(412).json({
+						msg: error.message
+					});
+				});
+		})
 
-  .post((req, res) => {
-    req.body.user_id = req.user.id;
-    Tasks.create(req.body)
-      .then(result => res.json(result))
-      .catch(error => {
-        res.status(412).json({
-          msg: error.message
-        });
-      });
-  });
+	.post((req, res) => {
+		req.body.user_id = req.user.id;
+		Tasks.create(req.body)
+			.then(result => res.json(result))
+			.catch(error => {
+				res.status(412).json({
+					msg: error.message
+				});
+			});
+	});
 
-  app.route("/tasks/:id")
-    .all(app.auth.authenticate())
-    .get((req, res) => {
-      Tasks.findOne({
-          where: {
-            id: req.param.id,
-            user_id: req.param.user.id
-          }
-        })
-        .then(result => {
-          if (result) {
-            res.json(result);
-          } else {
-            res.sendStatus(404);
-          }
-        })
-        .catch(error => {
-          res.status(412).json({
-            msg: error.message
-          });
-        });
-    })
+	app.route("/tasks/:id")
+		.all(app.auth.authenticate())
+		.get((req, res) => {
+			Tasks.findOne({
+					where: {
+						id: req.param.id,
+						user_id: req.param.user.id
+					}
+				})
+				.then(result => {
+					if (result) {
+						res.json(result);
+					} else {
+						res.sendStatus(404);
+					}
+				})
+				.catch(error => {
+					res.status(412).json({
+						msg: error.message
+					});
+				});
+		})
 
-  .put((req, res) => {
-    Tasks.update(req.boby, {
-        where: {
-          id: req.param.id,
-          user_id: req.param.user.id
-        }
-      })
-      .then(result => res.sendStatus(204))
-      .catch(error => {
-        res.status(412).json({
-          msg: error.message
-        });
-      });
-  })
+	.put((req, res) => {
+		Tasks.update(req.boby, {
+				where: {
+					id: req.param.id,
+					user_id: req.param.user.id
+				}
+			})
+			.then(result => res.sendStatus(204))
+			.catch(error => {
+				res.status(412).json({
+					msg: error.message
+				});
+			});
+	})
 
-  .delete((req, res) => {
-    Tasks.destroy({
-        where: {
-          id: req.param.id,
-          user_id: req.param.user.id
-        }
-      })
-      .then(result => res.sendStatus(204))
-      .catch(error => {
-        res.status(412).json({
-          msg: error.message
-        });
-      });
-  });
+	.delete((req, res) => {
+		Tasks.destroy({
+				where: {
+					id: req.param.id,
+					user_id: req.param.user.id
+				}
+			})
+			.then(result => res.sendStatus(204))
+			.catch(error => {
+				res.status(412).json({
+					msg: error.message
+				});
+			});
+	});
 };
