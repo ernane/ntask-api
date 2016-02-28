3964e2d7c238489c082209fd0ebf372f61dd21b9
Ernane Sena <ernane.sena@gmail.com>
2016-02-28 14:59:03 -0300

    Documentação Token

--- routes/token.js
+++ routes/token.js
@@ -3,7 +3,23 @@ import jwt from "jwt-simple";
 module.exports = app => {
   const cfg = app.libs.config;
   const Users = app.db.models.Users;
-
+  /**
+   * @api {post} /token Token Autenticado
+   * @apiGroup Crendencial
+   * @apiParam {String} email Email de usuário
+   * @apiParam {String} password Senha de usuário
+   * @apiParamExample {json} Entrada
+   *    {
+   *      "email": "jhon@ntask.com",
+   *      "password": "12345"
+   *    }
+   *  @apiSuccess {String} token Token de usuário authenticado
+   *  @apiSuccessExample {json} Sucesso
+   *    HTTP/1.1 200 OK
+   *    { "token": "xuan.ey.a81m3.014nsra" }
+   *  @apiErrorExample {json} Erro de autenticação
+   *    HTTP/1.1 401 Unauthorized
+   */
   app.post("/token", (req, res) => {
     if (req.body.email && req.body.password) {
       const email = req.body.email;
