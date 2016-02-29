1bf61a3c7fe960fb6c297e704fca2cccca062c23
Ernane Sena <ernane.sena@gmail.com>
2016-02-28 22:27:28 -0300

    Ajustes espaço

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -8,27 +8,27 @@ import logger from './logger.js';
 
 
 module.exports = app => {
-	app.set("port", 3000);
-	app.set("json spaces", 2);
-	app.use(morgan("common", {
-		stream: {
-			write: (message) => {
-				logger.info(message);
-			}
-		}
-	}));
-	app.use(helmet());
-	app.use(cors({
-		origin: ["http://localhost:3001"],
-		methods: ["GET", "POST", "PUT", "DELETE"],
-		allowedHeaders: ["Content-Type", "Authorization"]
-	}));
-	app.use(compression());
-	app.use(bodyParser.json());
-	app.use(app.auth.initialize());
-	app.use((req, res, next) => {
-		delete req.body.id;
-		next();
-	});
-	app.use(express.static('public'));
+  app.set("port", 3000);
+  app.set("json spaces", 2);
+  app.use(morgan("common", {
+    stream: {
+      write: (message) => {
+        logger.info(message);
+      }
+    }
+  }));
+  app.use(helmet());
+  app.use(cors({
+    origin: ["http://localhost:3001"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  }));
+  app.use(compression());
+  app.use(bodyParser.json());
+  app.use(app.auth.initialize());
+  app.use((req, res, next) => {
+    delete req.body.id;
+    next();
+  });
+  app.use(express.static('public'));
 };
